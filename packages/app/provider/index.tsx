a920c4ae80e4d7d63d578ae79d85b8ff3ec2327c
Jose Luis Rojas Aranda <joseluisrojasaranda@gmail.com>
2023-04-16 10:07:14 -0600

    animations ssr tests

---
+++ packages/app/provider/index.tsx
@@ -0,0 +1,31 @@
+import { CustomToast } from '@my/ui'
+import config from '../tamagui.config'
+import { NavigationProvider } from './navigation'
+import {
+  isServerSide,
+  TamaguiProvider,
+  TamaguiProviderProps,
+  ToastProvider,
+  ToastViewport,
+} from '@my/ui'
+import { useColorScheme } from 'react-native'
+
+export function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'config'>) {
+  const scheme = useColorScheme()
+  const defaultTheme = isServerSide ? undefined : scheme === 'dark' ? 'dark' : 'light'
+
+  console.log('isServerSide', isServerSide)
+  console.log('scheme', scheme)
+  console.log('defaultTheme', defaultTheme)
+
+  return (
+    <TamaguiProvider config={config} disableInjectCSS defaultTheme={defaultTheme} {...rest}>
+      <ToastProvider swipeDirection="horizontal" native="mobile">
+        <NavigationProvider>{children}</NavigationProvider>
+
+        <CustomToast />
+        <ToastViewport left={0} right={0} top={2} />
+      </ToastProvider>
+    </TamaguiProvider>
+  )
+}
